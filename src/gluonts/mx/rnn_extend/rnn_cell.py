9451d35ef6c344eacf5cd67000cf578e7e1ea2c8
unknown <xcgoner1108@gmail.com>
2020-07-08 20:53:51 -0500

    fix the implementation of zoneout for rnn

--- src/gluonts/mx/rnn_extend/rnn_cell.py
+++ src/gluonts/mx/rnn_extend/rnn_cell.py
@@ -68,7 +68,7 @@ class RNNZoneoutCell(ModifierCell):
         # use the same mask as output, instead of simply copy output to the first element
         # in case that the base cell is ResidualCell
         new_states = [F.where(output_mask, next_states[0], states[0])
-                          if p_outputs != 0. else next_states[0])]
+                          if p_outputs != 0. else next_states[0]]
         new_states += ([F.where(mask(p_states, new_s), new_s, old_s) for new_s, old_s in
                         zip(next_states[1:], states[1:])] if p_states != 0. else next_states[1:])
 
